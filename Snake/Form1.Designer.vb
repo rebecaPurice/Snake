25a37ec82c3c7cfdfb1e733c69c52e206fb634c6
rebecaPurice <rebe_ligia@yahoo.com>
2016-06-06 08:49:53 +0300

    Add files via upload

---
+++ Snake/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.pb_Field = New System.Windows.Forms.PictureBox()
+        Me.tm_snakeMover = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.pb_Field, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pb_Field
+        '
+        Me.pb_Field.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.pb_Field.Location = New System.Drawing.Point(14, 13)
+        Me.pb_Field.Name = "pb_Field"
+        Me.pb_Field.Size = New System.Drawing.Size(550, 279)
+        Me.pb_Field.TabIndex = 0
+        Me.pb_Field.TabStop = False
+        '
+        'tm_snakeMover
+        '
+        Me.tm_snakeMover.Interval = 50
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 308)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(288, 78)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Pentru a incepe jocul, apasati una dintre tastele A, S, D, W." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Stanga: A" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Dreapta" &
+    ": D" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Jos: S" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Sus: W" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Succes!"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(575, 394)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pb_Field)
+        Me.Name = "Form1"
+        Me.Text = "Snake Game"
+        CType(Me.pb_Field, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pb_Field As PictureBox
+    Friend WithEvents tm_snakeMover As Timer
+    Friend WithEvents Label1 As Label
+End Class
